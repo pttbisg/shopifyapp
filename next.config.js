26b8fd9f51914b3f09ba41babef7899a465bbdda
Aeyzaz <aeyzaz@hotmail.com>
2021-07-08 18:35:40 +0500

    updated host configs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { parsed: localEnv } = require("dotenv").config();
 
 const webpack = require("webpack");
 const apiKey = JSON.stringify(process.env.SHOPIFY_API_KEY);
-const host = JSON.stringify(process.env.SHOPIFY_APP_URL);
+const host = JSON.stringify(process.env.HOST);
 
 module.exports = {
   webpack: (config) => {
