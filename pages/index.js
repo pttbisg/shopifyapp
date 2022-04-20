7fcadee3570a129dafc4661b89fc6a703a6f2dbc
doantrungnghia <trungnghia260114@gmail.com>
2022-04-20 10:19:47 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ const Index = () => (
       <TextContainer>
         This app installs webhooks for order create, order edit and order delete
         events. It allows our local teams in country to process your orders with
-        our warehouses. To uninstall, go to Apps > Delete
+        our warehouses. To uninstall, go to Apps (on your Shopify Dashboard
+        Sidebar) > Delete
       </TextContainer>
     </Card>
   </Page>
